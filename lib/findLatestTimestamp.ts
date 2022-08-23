cdcb8ffd7d08a54fc0af5418135093d87fa96f7d
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2022-08-23 10:17:53 +0200

    fix: make sure function returns timestamp (number)

--- lib/findLatestTimestamp.ts
+++ lib/findLatestTimestamp.ts
@@ -4,10 +4,10 @@ import { fetchTimeStamp } from './fetchTimestamp.js'
 const getFilesUrl =
 	'https://api.github.com/repos/lenakh97/Nordic-strava-application/git/trees/gh-pages?recursive=1'
 
-export const findLatestTimestamp = async (): Promise<any> => {
+export const findLatestTimestamp = async (): Promise<number> => {
 	const responseFileFetching = await fetchFiles(getFilesUrl)
 	const fileTreeLength = responseFileFetching.tree.length
 	const lastFileName = responseFileFetching.tree[fileTreeLength - 1].path
-	const timestampLastFetch = (await fetchTimeStamp(lastFileName)).timeStamp
-	return timestampLastFetch
+	const timestampLastFetch = await fetchTimeStamp(lastFileName)
+	return timestampLastFetch.timestamp
 }
