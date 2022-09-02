159f375f66ca2e937da34a7a713b748a4687998f
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2022-09-02 15:31:43 +0200

    feat: write files to the folder for that week, make folder if it doesn't exist

--- fetch.ts
+++ fetch.ts
@@ -1,15 +1,27 @@
 import { promises as fs } from 'fs'
+import { mkdir } from 'fs/promises'
+import path from 'path'
 import { getStravaData } from './getStravaData.js'
+import { weekNumber } from './lib/getWeek.js'
 
 getStravaData()
-	.then(
-		async (data) =>
-			await fs.writeFile(
-				`./data/public/data-${new Date().toISOString()}.json`,
-				JSON.stringify(data, null, 2),
-				'utf-8',
-			),
-	)
+	.then(async (data) => {
+		const stravaDir = path.join(
+			process.cwd(),
+			'data',
+			`week-${weekNumber.toString().padStart(2, '0')}`,
+		)
+		try {
+			await mkdir(stravaDir)
+		} catch {
+			// directory exists
+		}
+		await fs.writeFile(
+			path.join(stravaDir, `data-${new Date().toISOString()}.json`),
+			JSON.stringify(data, null, 2),
+			'utf-8',
+		)
+	})
 	.catch((error) => {
 		console.error('Failed to write to shadow')
 		console.error(error)
